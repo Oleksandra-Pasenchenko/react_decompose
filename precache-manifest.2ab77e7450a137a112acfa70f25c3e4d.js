bc6830a2d8bd65b9c50071a178fa871f0d86432e
Oleksandra-Pasenchenko <pasenchenko.av97@gmail.com>
2021-09-20 14:56:04 +0300

    Updates

---
+++ precache-manifest.2ab77e7450a137a112acfa70f25c3e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e607feff68dcb6c3b68547adb08ee44",
+    "url": "./index.html"
+  },
+  {
+    "revision": "095bd948a704f896a434",
+    "url": "./static/css/main.8e0c2fa1.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095bd948a704f896a434",
+    "url": "./static/js/main.c1c04efc.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
